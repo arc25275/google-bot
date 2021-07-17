d4b7c843bf64a191f73164b41c54170aa1d966f8
arc25275 <arc25275@gmail.com>
2021-07-16 22:04:42 -0700

    Search Command

---
+++ commands/search.js
@@ -0,0 +1,40 @@
+fetch = require("node-fetch");
+const { searchkey } = require("../config/auth.json");
+const randColor = require("../util/randColor");
+module.exports = {
+	name: "search",
+	description: "Searches something",
+	options: [
+		{
+			name: "query",
+			type: "STRING",
+			description: "What you want to search",
+			required: true,
+		},
+	],
+	enabled: true,
+	permissions: [],
+	async run(client, interaction) {
+		await interaction.defer();
+		const { value: query } = interaction.options.get("query");
+		const fields = [];
+		await fetch(
+			`https://customsearch.googleapis.com/customsearch/v1?cx=336aaf40ae3497966&exactTerms=${query}&key=${searchkey}`
+		)
+			.then((res) => res.json())
+			.then((json) =>
+				json.items.forEach((res) => {
+					fields.push({
+						name: `**${res.title}** - ${res.link}`,
+						value: res.snippet,
+					});
+				})
+			);
+		const embed = {
+			title: "Results:",
+			fields: fields,
+			color: randColor(),
+		};
+		await interaction.editReply({ embeds: [embed] });
+	},
+};
